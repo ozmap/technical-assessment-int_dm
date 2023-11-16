c7ed18116f49fc5f7458a9bf87c42b282efbb267
Leonardo C. Brandao <leonardocbrand@gmail.com>
2023-11-16 14:54:41 -0300

    fix: directory

--- src/db/models.ts
+++ src/db/models.ts
@@ -3,12 +3,12 @@ import 'reflect-metadata';
 import * as mongoose from 'mongoose';
 import { TimeStamps } from '@typegoose/typegoose/lib/defaultClasses';
 import { pre, getModelForClass, Prop, Ref, modelOptions } from '@typegoose/typegoose';
-import lib from './lib';
 
 import ObjectId = mongoose.Types.ObjectId;
+import lib from '../lib';
 
 class Base extends TimeStamps {
-  @Prop({ required: true, default: () => (new ObjectId()).toString() })
+  @Prop({ required: true, default: () => new ObjectId().toString() })
   _id: string;
 }
 
