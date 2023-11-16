c905f631a728d616b096d3f8a730d5b69233908d
Leonardo C. Brandao <leonardocbrand@gmail.com>
2023-11-16 18:35:35 -0300

    feat: joi schemas

---
+++ src/middlewares/joi.schemas.ts
@@ -0,0 +1,14 @@
+import Joi = require('joi');
+
+export const userSchema = Joi.object({
+  name: Joi.string().label('name').required(),
+  email: Joi.string().email().label('email').required(),
+  address: Joi.string().label('address'),
+  coordinates: Joi.array().items(Joi.number(), Joi.number()).label('coordinates'),
+})
+  .oxor('address', 'coordinates')
+  .messages({
+    'any.required': 'É necessário o campo {{#label}}',
+    'string.email': 'Formato de email inválido',
+    'object.oxor': 'Informe apenas address ou coordinates',
+  });
