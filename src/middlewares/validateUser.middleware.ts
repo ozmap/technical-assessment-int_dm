05481f65827a7b805eeb666b3c80d2d6456baf0b
Leonardo C. Brandao <leonardocbrand@gmail.com>
2023-11-17 02:03:51 -0300

    fix: validateUser middleware

--- src/middlewares/validateUser.middleware.ts
+++ src/middlewares/validateUser.middleware.ts
@@ -1,16 +1,33 @@
 import { NextFunction, Request, Response } from 'express';
 import customError from '../errors/error';
-import { UserRequestBody } from '../types';
 import { userSchema } from './joi.schemas';
+import { UserRequestBody } from '../types/user.types';
 
 const validateUser = (req: Request, _res: Response, next: NextFunction) => {
   try {
     const user: UserRequestBody = req.body;
 
+    if (!user.coordinates && !user.address) {
+      throw customError({
+        name: 'BAD_REQUEST',
+        statusCode: 400,
+        message: 'É necessário o endereço ou as coordenadas',
+      });
+    }
+
+    if (user.coordinates) {
+      if (typeof user.coordinates[0] !== 'number' || typeof user.coordinates[1] !== 'number') {
+        throw customError({
+          name: 'BAD_REQUEST',
+          statusCode: 400,
+          message: 'Informe coordenadas com latitude e longitude em formato numérico',
+        });
+      }
+    }
+
     const { error } = userSchema.validate(user);
 
     if (error) {
-      console.log(error);
       throw customError({
         name: 'BAD_REQUEST',
         statusCode: 400,
