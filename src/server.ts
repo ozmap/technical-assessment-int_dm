0454141644de5423f7c3579f43092808bc7d1e4c
Leonardo C. Brandao <leonardocbrand@gmail.com>
2023-11-21 14:42:50 -0300

    feat: logger

--- src/server.ts
+++ src/server.ts
@@ -3,19 +3,23 @@ import './db/database';
 import errorMiddleware from './middlewares/error.middleware';
 import usersRoute from './routes/users.route';
 import regionsRoute from './routes/regions.route';
+import exportRoute from './routes/export.route';
 import swaggerUi from 'swagger-ui-express';
 import swaggerDocs from './swagger.json';
+import logger from './middlewares/logger.middleware';
 
 const PORT = process.env.API_PORT || 3002;
 
 const server = express();
 
 server.use(express.json());
+server.use(logger);
 
 server.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 server.use('/users', usersRoute);
 server.use('/regions', regionsRoute);
+server.use('/export', exportRoute);
 
 server.use(errorMiddleware);
 
