c120ec9e61cc816fc9d7cf928358f82f705b731f
marceloabbadia <marcelo.abbadia@gmail.com>
2023-11-23 16:01:32 -0300

    configuracao dockerfile e docker-compose

--- api/src/index.ts
+++ api/src/index.ts
@@ -7,7 +7,7 @@ import userRoute from './routes/user.route'
 import regionRoute from './routes/regions.route'
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT
 
 env.config()
 init()
